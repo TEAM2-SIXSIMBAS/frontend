f445525565f4c511ede96aaa33f36aa5185e14b0
syj4688 <syj4688@naver.com>
2025-08-13 23:31:34 +0900

    250813 InfoDetail

---
+++ src/components/NaviBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../components/NaviBar.css";
+
+export default function NaviBar() {
+    return (
+        <nav className="navi-bar">
+            <ul>
+                <li>제휴 정보</li>
+                <li>제휴 지도</li>
+                <li>제휴사 정보</li>
+            </ul>
+        </nav>
+    );
+}
