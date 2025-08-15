4317124235e97620fc844531e47f76f76cb560cf
seohyun <westhyun1107@gmail.com>
2025-08-15 13:49:33 +0900

    충돌 해결(첫 화면 변경, 라우 팅 변경, 메인 화면에서 카드 클릭시 상세 정보 창 연결, 리뷰 작성 버튼과 리뷰 작성 창 연결 / NaviBar 통일 필요)

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-import { StrictMode, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ReviewModal from './pages/ReviewWrite/ReviewModal.jsx'
-
-function PreviewRoot() {
-  // 처음 진입 시 바로 모달 열어보기
-  const [open, setOpen] = useState(true)
-
-  return (
-    <>
-      <App />
-      {open && <ReviewModal onClose={() => setOpen(false)} />}
-    </>
-  )
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PreviewRoot />
+    <App />
   </StrictMode>,
 )
